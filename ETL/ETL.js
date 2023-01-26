fbba28ae4fc8495f87a7cd90ed08fc10c9eaf8e4
Jack Bossert <jackbossert@Jacks-MacBook-Pro.local>
2023-01-26 16:04:45 -0700

    Fixed ETL

--- ETL/ETL.js
+++ ETL/ETL.js
@@ -1,45 +1,49 @@
 const { Client } = require('pg');
 const fs = require('fs');
 const { parse } = require('csv-parse');
-const { format } = require('date-fns');
 
 const client = new Client({ database: 'reviews' });
 
 client.connect();
 
-const parser = {
-  products: parse({columns: true}, function (err, data) {
-    var promises = [];
-    while(data.length !== 0) {
-      var unit = data.shift();
-      console.log('PARSED : ' + unit.id);
-      promises.push(new Promise((resolve, reject) => {
-        var text = 'INSERT INTO Meta(id, name) VALUES($1, $2)';
-        var values = [unit.id, unit.name];
-        client.query(text, values, (err, res) => {
-          if (err) reject(err);
-          else{
-            console.log('QUERIED : ' + unit.id);
-            resolve(res);
-          }
-        });
-      }));
-    }
-    Promise.all(promises)
-      .then((values) => {
-        console.log(values.length + ' INSERTED');
-      })
-      .catch((err) => {
-        console.log(err);
-        client.end();
+var productsParser = parse({columns: true}, function (err, data) {
+  if (err) throw err;
+  var promises = [];
+  while(data.length !== 0) {
+    var unit = data.shift();
+    console.log('PARSED product_id ' + unit.id);
+    promises.push(new Promise((resolve, reject) => {
+      var text = 'INSERT INTO Meta(id, name) VALUES($1, $2)';
+      var values = [unit.id, unit.name];
+      client.query(text, values, (err, res) => {
+        if (err) reject(err);
+        else{
+          console.log('INSERTED product_id ' + values[0]);
+          resolve(res);
+        }
       });
-  }),
+    }));
+  }
+  Promise.all(promises)
+    .then(() => {
+      delete promises;
+      fs.createReadStream(__dirname + '/data/reviews.csv').pipe(reviewsParser);
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+});
 
-  reviews: parse({columns: true}, function (err, data) {
+var reviewsParser = parse({columns: true}, function (err, data) {
+  console.log('here');
+  if (err) throw err;
+  var partialParser = () => {
     var promises = [];
-    while(data.length !== 0) {
+    console.log('here');
+    for (let i = 0 ; i < 50000 && data.length !== 0; i++) {
       var unit = data.shift();
-      console.log('PARSED : ' + unit.id);
+      console.log('PARSED review_id ' + unit.id);
       promises.push(new Promise((resolve, reject) => {
         var text = `INSERT INTO Reviews
         (id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
@@ -48,7 +52,7 @@ const parser = {
           unit.id,
           unit.product_id,
           unit.rating,
-          (new Date(unit.date)).toString(),
+          (new Date(parseInt(unit.date))),
           unit.summary,
           unit.body,
           unit.recommend,
@@ -61,197 +65,120 @@ const parser = {
         client.query(text, values, (err, res) => {
           if (err) reject(err);
           else{
-            console.log('QUERIED : ' + unit.id);
+            console.log('INSERTED review_id ' + values[0]);
             resolve(res);
           }
         });
       }));
     }
     Promise.all(promises)
-      .then((values) => {
-        console.log(values.length + ' INSERTED');
+      .then(() => {
+        delete promises;
+        if (data.length !== 0) { partialParser(); }
+        else { fs.createReadStream(__dirname + '/data/reviews_photos.csv').pipe(photosParser); }
       })
       .catch((err) => {
         console.log(err);
         client.end();
       });
-  }),
+  };
+  partialParser();
+});
 
-  photos: parse({columns: true}, function (err, data) {
-    var promises = [];
-    while(data.length !== 0) {
-      var unit = data.shift();
-      console.log('PARSED : ' + unit.id);
-      promises.push(new Promise((resolve, reject) => {
-        var text = 'INSERT INTO Photos(id, review_id, url) VALUES($1, $2, $3)';
-        var values = [unit.id, unit.review_id, unit.url];
-        client.query(text, values, (err, res) => {
-          if (err) reject(err);
-          else{
-            console.log('QUERIED : ' + unit.id);
-            resolve(res);
-          }
-        });
-      }));
-    }
-    Promise.all(promises)
-      .then((values) => {
-        console.log(values.length + ' INSERTED');
-      })
-      .catch((err) => {
-        console.log(err);
-        client.end();
+var photosParser = parse({columns: true}, function (err, data) {
+  if (err) throw err;
+  var promises = [];
+  while(data.length !== 0) {
+    var unit = data.shift();
+    console.log('PARSED photo_id ' + unit.id);
+    promises.push(new Promise((resolve, reject) => {
+      var text = 'INSERT INTO Photos(id, review_id, url) VALUES($1, $2, $3)';
+      var values = [unit.id, unit.review_id, unit.url];
+      client.query(text, values, (err, res) => {
+        if (err) reject(err);
+        else{
+          console.log('INSERTED photo_id ' + values[0]);
+          resolve(res);
+        }
       });
-  }),
+    }));
+  }
+  Promise.all(promises)
+    .then(() => {
+      delete promises;
+      fs.createReadStream(__dirname + '/data/characteristics.csv').pipe(characteristicsParser);
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+});
 
-  characteristics: parse({columns: true}, function (err, data) {
-    var promises = [];
-    while(data.length !== 0) {
-      var unit = data.shift();
-      console.log('PARSED : ' + unit.id);
-      promises.push(new Promise((resolve, reject) => {
-        var text = 'INSERT INTO Characteristics(id, product_id, name) VALUES($1, $2, $3)';
-        var values = [unit.id, unit.product_id, unit.name];
-        client.query(text, values, (err, res) => {
-          if (err) reject(err);
-          else{
-            console.log('QUERIED : ' + unit.id);
-            resolve(res);
-          }
-        });
-      }));
-    }
-    Promise.all(promises)
-      .then((values) => {
-        console.log(values.length + ' INSERTED');
-      })
-      .catch((err) => {
-        console.log(err);
-        client.end();
+var characteristicsParser = parse({columns: true}, function (err, data) {
+  if (err) throw err;
+  var promises = [];
+  while(data.length !== 0) {
+    var unit = data.shift();
+    console.log('PARSED characteristic_id ' + unit.id);
+    promises.push(new Promise((resolve, reject) => {
+      var text = 'INSERT INTO Characteristics(id, product_id, name) VALUES($1, $2, $3)';
+      var values = [unit.id, unit.product_id, unit.name];
+      client.query(text, values, (err, res) => {
+        if (err) reject(err);
+        else{
+          console.log('INSERTED characteristic_id ' + values[0]);
+          resolve(res);
+        }
       });
-  }),
+    }));
+  }
+  Promise.all(promises)
+    .then(() => {
+      delete promises;
+      fs.createReadStream(__dirname + '/data/characteristic_reviews.csv').pipe(charRevParser);
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+});
 
-  char_rev: parse({columns: true}, function (err, data) {
-    var promises = [];
-    while(data.length !== 0) {
-      var unit = data.shift();
-      console.log('PARSED : ' + unit.id);
-      promises.push(new Promise((resolve, reject) => {
-        var text = `INSERT INTO
-        Characteristics_Reviews(id, characteristic_id, review_id, value)
-        VALUES($1, $2, $3, $4)`;
-        var values = [
-          unit.id,
-          unit.characteristic_id,
-          unit.review_id,
-          unit.value
-        ];
-        client.query(text, values, (err, res) => {
-          if (err) reject(err);
-          else{
-            console.log('QUERIED : ' + unit.id);
-            resolve(res);
-          }
-        });
-      }));
-    }
-    Promise.all(promises)
-      .then((values) => {
-        console.log(values.length + ' INSERTED');
-        client.end();
-      })
-      .catch((err) => {
-        console.log(err);
-        client.end();
+var charRevParser = parse({columns: true}, function (err, data) {
+  if (err) throw err;
+  var promises = [];
+  while(data.length !== 0) {
+    var unit = data.shift();
+    console.log('PARSED characteristics_reviews_id ' + unit.id);
+    promises.push(new Promise((resolve, reject) => {
+      var text = `INSERT INTO
+      Characteristics_Reviews(id, characteristic_id, review_id, value)
+      VALUES($1, $2, $3, $4)`;
+      var values = [
+        unit.id,
+        unit.characteristic_id,
+        unit.review_id,
+        unit.value
+      ];
+      client.query(text, values, (err, res) => {
+        if (err) reject(err);
+        else{
+          console.log('INSERTED characteristics_reviews_id ' + values[0]);
+          resolve(res);
+        }
       });
-  })
-};
+    }));
+  }
+  Promise.all(promises)
+    .then(() => {
+      delete promises;
+      console.log();
+      console.log('====== ETL COMPLETED ======');
+      client.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+});
 
-async function ETL() {
-  // const p = await fs.createReadStream(__dirname + '/testData/products.csv').pipe(parser.products);
-  // const r = await fs.createReadStream(__dirname + '/testData/reviews.csv').pipe(parser.reviews);
-  // const rp = await fs.createReadStream(__dirname + '/testData/reviews_photos.csv').pipe(parser.photos);
-  // const c = await fs.createReadStream(__dirname + '/testData/characteristics.csv').pipe(parser.characteristics);
-  const cr = await fs.createReadStream(__dirname + '/testData/characteristics_reviews.csv').pipe(parser.char_rev);
-
-
-  // fs.createReadStream(__dirname + '/data/products.csv').pipe(parser.products);
-  // fs.createReadStream(__dirname + '/data/reviews.csv').pipe(parser.reviews);
-  // fs.createReadStream(__dirname + '/data/reviews_photos.csv').pipe(parser.photos);
-  // fs.createReadStream(__dirname + '/data/characteristics.csv').pipe(parser.characteristics);
-  // fs.createReadStream(__dirname + '/data/characteristics_reviews.csv').pipe(parser.characteristics);
-}
-
-ETL();
-console.log('DONE');
-// client.end();
-// var metaParser = parse({columns: true}, function (err, data) {
-//   var promises = [];
-//   while(data.length !== 0) {
-//     var unit = data.shift();
-//     console.log('PARSED : ' + unit.id);
-//     promises.push(new Promise((resolve, reject) => {
-//       var text = 'INSERT INTO Meta(id, name) VALUES($1, $2)';
-//       var values = [unit.id, unit.name];
-//       client.query(text, values, (err, res) => {
-//         if (err) reject(err);
-//         else{
-//           console.log('QUERIED : ' + unit.id);
-//           resolve(res);
-//         }
-//       });
-//     }));
-//   }
-//   Promise.all(promises)
-//     .then((values) => {
-//       console.log(values.length + ' INSERTED');
-//       client.end();
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       client.end();
-//     });
-// });
-
-// var reviewParser = parse({columns: true}, function (err, data) {
-//   var promises = [];
-//   while(data.length !== 0) {
-//     var unit = data.shift();
-//     console.log('PARSED : ' + unit.id);
-//     promises.push(new Promise((resolve, reject) => {
-//       var text = `INSERT INTO Reviews
-//       (id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
-//       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`;
-//       var values = [
-//         unit.id,
-//         unit.product_id,
-//         unit.rating,
-//         unit.date,
-//         unit.summary,
-//         unit.body,
-//         unit.recommend,
-//         unit.reported,
-//         unit.reviewer_name,
-//         unit.reviewer_email,
-//         unit.response,
-//         unit.helpfulness
-//       ];
-//       client.query(text, values, (err, res) => {
-//         if (err) reject(err);
-//         else{
-//           console.log('QUERIED : ' + unit.id);
-//           resolve(res);
-//         }
-//       });
-//     }));
-//   }
-//   Promise.all(promises)
-//     .then((values) => {
-//       console.log(values.length + ' INSERTED');
-//       client.end();
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       client.end();
-//     });
-// });
+fs.createReadStream(__dirname + '/data/reviews.csv').pipe(reviewsParser);
